7f9c2d7e961312b4806065f93bb519987f45fbba
Divesh Patel <Diweshpatelsinha@gmail.com>
2024-11-06 03:48:21 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,16 @@
+switch (3) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("february");
+        break;
+    case 3:
+        console.log("March");
+        break;
+
+    default:
+        console.log("default case matched");
+        
+        break;
+}
