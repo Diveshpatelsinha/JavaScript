9aa6ad8668fa6e3014ba28b4cbacf7b723b902bc
Divesh Patel <Diweshpatelsinha@gmail.com>
2024-11-07 07:34:52 +0000

    whileloop

---
+++ 05_iterations/two.js
@@ -0,0 +1,14 @@
+let myArray = ['batman', "superman", "spiderman", "ironman" ]
+
+let arr = 0
+
+while (arr < myArray.length) {
+    console.log(`superhero is ${myArray[arr]}`);
+    arr=arr+1
+    
+}
+
+let a=0
+console.log(`value is ${a}`);
+
+
