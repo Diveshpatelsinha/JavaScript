060f7b8fb3af8ee275bca1bbd36b3ed3d9f960be
Divesh Patel <Diweshpatelsinha@gmail.com>
2024-11-18 09:14:45 +0000

    arrays 1

--- arrays.js
+++ arrays.js
@@ -1,4 +1,52 @@
-let arrays = [23, 34, 45, 56, 'diwesh']
+/*let arrays = [23, 34, 45, 56, 'diwesh']
 console.log(arrays[4]);
-console.log(arrays.length);
+console.log(arrays.length);*/
+
+//methods in arrays
+/*let num = [1,2,3,4,5]
+let b = num.toString()
+console.log(b, typeof b);
+
+let c = num.join("-")
+console.log(c, typeof c);
+//let r = num.pop()
+//console.log(num);
+
+//let r = num.push(7)
+//console.log(num, r);
+
+//let r = num.shift()
+//console.log(r, num);
+
+//some more methods
+
+//delete num[0]
+//console.log(num, num.length);
+
+let num2 = [ 11,16,56,7,8,9,10]
+//let newarray = num.concat(num2)
+//console.log(newarray);
+
+let compare = (a,b) => {
+    return a-b
+}
+num2.sort(compare)
+//num2.reverse()
+console.log(num2);*/
+
+//splice and slice
+
+let a = [ 1,2,3,4,5,6]
+//let deletedvalues = a.splice(2, 2, 7,8,9)
+//console.log(a);
+//console.log(deletedvalues);
+
+let b = a.slice(2, 5)
+console.log(b);
+
+
+
+
+
+
 
