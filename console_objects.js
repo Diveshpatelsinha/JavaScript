3ccd07326bcd534be1624e8f5b873ef6995d0ddb
Divesh Patel <Diweshpatelsinha@gmail.com>
2024-11-19 16:18:33 +0000

    console objects

---
+++ console_objects.js
@@ -0,0 +1,10 @@
+obj = {a:1, b:2, c:3}
+console.table(obj)
+
+
+console.warn('hey dont do this')
+
+console.info("this is a console object")
+console.error("error occured")
+console.assert( "err" != false)
+console.assert( "err" == false)
