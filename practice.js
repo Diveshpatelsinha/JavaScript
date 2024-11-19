e9a05a9a087d48f81bc425c54711ee753ec8f6fc
Divesh Patel <Diweshpatelsinha@gmail.com>
2024-11-19 08:54:43 +0000

    practice questions

---
+++ practice.js
@@ -0,0 +1,49 @@
+//practice problem 1
+
+
+// let arr = [1, 3, 4, 5, 6, 2, 7, 8];
+
+// let a = prompt("Enter a number: ")
+// a = Number.parseInt(a)
+// arr.push(a)
+ 
+// console.log(arr)
+
+//practice problem 2
+
+// let arr = [1, 3, 4, 5, 6, 2, 7, 8];
+// let a;
+// do {
+//  a = prompt("Enter a number: ")
+// a = Number.parseInt(a)
+// arr.push(a)
+
+// } while (a !=0); 
+
+// console.log(arr)
+
+
+//practice problem 3
+
+// let arr = [1, 30, 4, 5, 6, 20, 7, 80];
+//  let b = arr.filter((a)=>{
+//     return a%10==0
+//  })
+//  console.log(b);
+
+
+//practice problem 4
+
+// let arr = [1, 3, 4, 5, 6, 2, 7, 8];
+// let a = arr.map((value)=>{
+//  return value*value
+// })
+// console.log(a);
+
+//practice problem 5
+
+// let arr = [1,2,3];
+// let a = arr.reduce((b,c)=>{
+//  return b*c
+// })
+// console.log(a);
