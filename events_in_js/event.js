ccce0ba1a4a7efeb7a853b3b2f51be8a0a47119b
Divesh Patel <Diweshpatelsinha@gmail.com>
2024-11-25 10:12:47 +0000

    events in js

---
+++ events_in_js/event.js
@@ -0,0 +1,20 @@
+
+
+let x = function(e) {
+    alert("hello world!")
+    }
+
+btn.addEventListener('click', x)
+
+// let y = function(e) {
+//     alert("hello world!2")
+//     }
+
+// btn.addEventListener('Click', y)
+
+// let a = prompt("click on 1");
+
+// if(a == '1'){
+//     btn.addEventListener("click", x)
+// }
+
