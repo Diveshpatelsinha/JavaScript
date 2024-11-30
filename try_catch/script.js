f044d44b10301463927c10ea8d4799b57a646cb0
Divesh Patel <Diweshpatelsinha@gmail.com>
2024-11-30 06:38:37 +0000

    try and catch

---
+++ try_catch/script.js
@@ -0,0 +1,35 @@
+setTimeout(() => {
+    console.log('Getting user instagram details... Please Wait....');
+    
+}, 2000);
+
+setTimeout(() => {
+    console.log('Fetching User ID and Password... Please Wait....');
+    
+}, 4000);
+
+try {
+    console.log(aditya);
+    
+} catch (error) {
+    console.log(error);
+    
+}
+
+setTimeout(() => {
+    console.log('user Id and password is fetched.... Please wait...');
+    
+}, 6000);
+
+setTimeout(() => {
+    console.log(`User ID is _aditya_13k `)
+    
+}, 8000);
+setTimeout(() => {
+    console.log(`User used Mob. is 82185479**`);
+    
+}, 10000);
+setTimeout(() => {
+    console.log('User Password is ***********');
+    
+}, 11000);
